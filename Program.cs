4efeaf0e31668ca403b0967549048cf0b71b86c4
Gavin-Fetrow-Student <gafe134@student.oaisd.org>
2024-10-25 12:13:51 +0000

    Made functions to complete different tasks

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 
 public class Program
 {
@@ -6,12 +8,15 @@ public class Program
         FahrenheitToCelsius(68); // Test FahrenheitToCelsius Function with a random value
 
         // Test your functions by calling them below:
-        // CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
-        // CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
-        // CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
-        // ...
-        // ...
-        // DisplayDayOfWeek(0); // This should output: "Thursday"
+        CheckForPositiveNegativeZero(-1); // This should output: "Your number is negative"
+        CheckForPositiveNegativeZero(0); // This should output: "Your number is zero"
+        CheckForPositiveNegativeZero(1); // This should output: "Your number is positive"
+        FindMinimum(4, 11, 6); // ...
+        FindMaximum(30, 22, 47); // ...
+        IsDivisibleBy5(31);
+        CheckEvenOrOdd(18);
+        CheckVowelOrConsonant('b');
+        DisplayDayOfWeek(4); // This should output: "Thursday"
 
     }
 
@@ -22,5 +27,132 @@ public class Program
     }
 
     // Define your functions below:
+    public static void CheckForPositiveNegativeZero (int number)
+    {
+        if (number < 0)
+        {
+            Console.WriteLine("Your number is negative.");
+        }
+        else if (number > 0)
+        {
+            Console.WriteLine("Your number is positive.");
+        }
+        else
+        {
+            Console.WriteLine("Your number is zero.");
+        }
+    }
 
+    public static void FindMinimum (int num1, int num2, int num3)
+    {
+        // num1 is the smallest if:
+        // num1 is less than or equal to num2, AND num1 is less than or equal to num3
+        // 7 8 9
+        // 7 7 8
+        // 8 7 8
+        // 7 7 6
+        // 9 9 9
+        if (num1 <= num2 && num1 <=num3)
+        {
+            Console.WriteLine("The minimum value is " + num1 + ".");
+        }
+        else if (num2 <= num1 && num2 <= num3)
+        {
+            Console.WriteLine("The minimum value is " + num2 + ".");
+        }
+        else
+        {
+            Console.WriteLine("The minimum value is " + num3 + ".");
+        }
+    }
+
+    public static void FindMaximum (int num1, int num2, int num3)
+    {
+        if (num1 >= num2 && num1 >= num3)
+        {
+            Console.WriteLine("The maximum value is " + num1 + ".");
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            Console.WriteLine("The maximum value is " + num2 + ".");
+        }
+        else
+        {
+            Console.WriteLine("The maximum value is " + num3 + ".");
+        }
+    }
+
+    public static void IsDivisibleBy5 (int number)
+    {
+        if (number % 5 == 0)
+        {
+            Console.WriteLine(number + " is divisible by 5.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not divisible by 5.");
+        }
+    }
+
+    public static void CheckEvenOrOdd (int number)
+    {
+        if (number % 2 == 0)
+        {
+            Console.WriteLine(number + " is an even number.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is an odd number.");
+        }
+    }
+
+    public static void CheckVowelOrConsonant (char letter)
+    {
+        switch (letter)
+        {
+            case 'a':
+            case 'A':
+            case 'e':
+            case 'E':
+            case 'i':
+            case 'I':
+            case 'o':
+            case 'O':
+            case 'u':
+            case 'U':
+                Console.WriteLine(letter + " is a vowel.");
+                break;
+            default:
+                Console.WriteLine(letter + " is a consonant.");
+                break;
+        }
+    }
+
+    public static void DisplayDayOfWeek (int dayCode)
+    {
+        switch (dayCode)
+        {
+        case 0:
+            Console.WriteLine("Sunday");
+            break;
+        case 1:
+            Console.WriteLine("Monday");
+            break;
+        case 2:
+            Console.WriteLine("Tuesday");
+            break;
+        case 3:
+            Console.WriteLine("Wednesday");
+            break;
+        case 4:
+            Console.WriteLine("Thursday");
+            break;
+        case 5:
+            Console.WriteLine("Friday");
+            break;
+        case 6:
+            Console.WriteLine("Saturday");
+            break;
+        }
+    }
 }
